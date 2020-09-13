426482f7871af81fbac786ee2584a36b5ec48ac1
curious-jp <curious@CURIOUS-DESKTOP.localdomain>
2020-09-13 14:56:20 +0900

    test2

--- bs-usd.py
+++ bs-usd.py
@@ -6,3 +6,5 @@ url="https://info.finance.yahoo.co.jp/history/?code=USDJPY"
 soup=BeautifulSoup(req.urlopen(url),'html.parser')
 price=soup.select_one(".stoksPrice").string
 print(price)
+
+#this is test
